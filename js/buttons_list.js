af66b419b5daec77d6a824e9faba4b3bc6071e6f
moze9lol <moze9lol@gmail.com>
2022-05-06 18:13:59 +0500

    feat: setup eng keys

--- js/buttons_list.js
+++ js/buttons_list.js
@@ -1,6 +1,43 @@
 export default [
-    ["1row"],
-    ["2row"],
-    ["3row"],
-    ["4row"]
+    {
+        elements: {
+            main: null,
+            keysContainer: null,
+            keys: []
+        },
+
+        eventHandlers: {
+            oninput: null,
+            onclose: null
+        },
+
+        properties: {
+            value: "",
+            capsLock: false
+        }
+
+        init() {
+
+        },
+
+        _createKeys() {
+
+        },
+
+        _triggerEvent(handlerName) {
+            console.log("Event Triggered! Event Name: " + handlerName);
+        },
+
+        _toggleCapsLock() {
+            console.log("Caps Lock Toggled!")
+        },
+
+        open(initialValue, oninput, onclose) {
+
+        },
+
+        close() {
+
+        }
+    }
 ];
--- js/index.js
+++ js/index.js
@@ -1,7 +1,74 @@
-import buttonList from "./buttons_list.js";
+// import buttonList from "./buttons_list.js";
 
-let str = "jija";
+const KEYBOARD = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
 
-console.log(buttonList, str);
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
 
+    properties: {
+        value: "",
+        capsLock: false
+    },
+
+    init() {
+        this.elements.main = document.createElement("div");
+        this.elements.keysContainer = document.createElement("div");
+
+        this.elements.main.classList.add("keyboard", "-keyboard--hidden");
+        this.elements.keysContainer.classList.add("keyboard--keys");
+
+        this.elements.main.appendChild(this.elements.keysContainer);
+        document.body.appendChild(this.elements.main);
+    },
+
+    _createKeys() {
+        const FRAGMENT = document.createDocumentFragment();
+        const keyLayout = [
+            "&grave;","1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
+            "Tab","q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "&bsol;", "Del",
+            "Caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter",
+            "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "ArrowUp","Shift",
+            "Ctrl", "Win", "Alt", "space", "Alt", "ArrowLeft", "ArrowDown", "ArrowRight", "Ctrl" 
+        ];
+
+        const createIconHTML = (icon_name) => {
+            return `<i class="material-icons">${icon_name}</i>`
+        }
+        
+        keyLayout.forEach(key => {
+            const KEY_ELEMENT = document.createElement("button");
+            const INSERT_LB = ["Backspace", "Del", "Enter", "Shift"].indexOf(key) !== -1;
+
+            KEY_ELEMENT.setAttribute("type", "button");
+            KEY_ELEMENT.classList.add("keyboard__key");
+        })
+    },
+
+    _triggerEvent(handlerName) {
+        console.log("Event Triggered! Event Name: " + handlerName);
+    },
+
+    _toggleCapsLock() {
+        console.log("Caps Lock Toggled!")
+    },
+
+    open(initialValue, oninput, onclose) {
+
+    },
+
+    close() {
+
+    }
+};
+
+window.addEventListener("DOMContentLoaded", function() {
+    KEYBOARD.init();
+});
 
